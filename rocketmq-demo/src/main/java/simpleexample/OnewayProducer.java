ea3f161ed2f55a33183dc1603e1be4f30d55a869
ipofss <274694451@qq.com>
2021-01-30 11:55:50 +0800

    typo

--- rocketmq-demo/src/main/java/simpleexample/OnewayProducer.java
+++ rocketmq-demo/src/main/java/simpleexample/OnewayProducer.java
@@ -9,27 +9,30 @@ import org.apache.rocketmq.remoting.exception.RemotingException;
 import java.io.UnsupportedEncodingException;
 
 /**
- * 单向传递消息
+ * 单向发送消息
+ * 这种方式主要用在不特别关系发送结果的场景，例如日志发送。
  *
  * @author: wangbingshuai
  * @create: 2020-07-11 19:21
  **/
 public class OnewayProducer {
     public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException {
-        // Instantiate with a producer group name.
+        // 实例化消息生产者Producer
         DefaultMQProducer producer = new DefaultMQProducer("wbs_mq_demo");
-        // Specify name server addresses.
+        // 设置NameServer的地址
         producer.setNamesrvAddr("localhost:9876");
-        // Launch the instance.
+        // 启动Producer实例
         producer.start();
         for (int i = 0; i < 100; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
             Message msg = new Message("TopicTest",
                     "TagA",
                     ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
-            // Call send message to deliver message to one of brokers.
+            // 发送单向消息，没有任何返回结果
             producer.sendOneway(msg);
         }
         Thread.sleep(5000);
+        // 如果不再发送消息，关闭Producer实例
         producer.shutdown();
     }
 }
--- rocketmq-demo/src/main/java/simpleexample/SyncProducer.java
+++ rocketmq-demo/src/main/java/simpleexample/SyncProducer.java
@@ -11,29 +11,32 @@ import org.apache.rocketmq.remoting.exception.RemotingException;
 import java.io.UnsupportedEncodingException;
 
 /**
- * 同步发送消息
+ * Producer端发送同步消息
+ * 这种可靠性同步地发送方式使用的比较广泛，比如：重要的消息通知，短信通知。
  *
  * @author: wangbingshuai
  * @create: 2020-07-10 20:38
  **/
 public class SyncProducer {
     public static void main(String[] args) throws MQClientException, UnsupportedEncodingException, RemotingException, InterruptedException, MQBrokerException {
-        // Instantiate with a producer group name.
+        // 实例化消息生产者Producer
         DefaultMQProducer producer = new DefaultMQProducer("wbs_mq_demo");
-        // Specify name server addresses.
+        // 设置NameServer的地址
         producer.setNamesrvAddr("localhost:9876");
-        // Launch the instance.
+        // 启动Producer实例
         producer.start();
         for (int i = 0; i < 100; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
             Message message = new Message("TopicTest",
                     "TagA",
-                    ("Hello RocketMQ" + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
             );
-            // Call send message to deliver message to one of brokers.
+            // 发送消息到一个Broker
             SendResult sendResult = producer.send(message);
+            // 通过sendResult返回消息是否成功送达
             System.out.printf("%s%n", sendResult);
         }
-        // Shut down once the producer instance is not longer in use.
+        // 如果不在发送消息，关闭Producer实例
         producer.shutdown();
     }
 }
