90be6391c469bf7f3e068673843bce591d1ed512
ipofss <274694451@qq.com>
2021-02-02 12:53:18 +0800

    延时消息

---
+++ rocketmq-demo/src/main/java/scheduleexample/ScheduleMessageConsumer.java
@@ -0,0 +1,40 @@
+package scheduleexample;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 延时消费者等待传入订阅消息
+ */
+public class ScheduleMessageConsumer {
+    public static void main(String[] args) throws MQClientException {
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("wbs_schedule_mq_demo");
+        consumer.setNamesrvAddr("localhost:9876");
+        // 订阅Topics
+        consumer.subscribe("TopicTest", "*");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        // 注册消息监听者
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messages, ConsumeConcurrentlyContext context) {
+                for (MessageExt message : messages) {
+                    // 打印近似延迟时间
+                    System.out.println("Receive message[msgId=" + message.getMsgId() + "] " + (System.currentTimeMillis() - message.getStoreTimestamp()) + "ms later, currentTimeLong=" + System.currentTimeMillis() + ", currentTime=" + sdf.format(new Date()) + ", message=" + new String(message.getBody()) + ", storeTimeLong=" + message.getStoreTimestamp() + ", bornTimeLong=" + message.getBornTimestamp());
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 启动消费者
+        consumer.start();
+        System.out.println("Consumer started.");
+    }
+}
---
+++ rocketmq-demo/src/main/java/scheduleexample/ScheduleMessageProducer.java
@@ -0,0 +1,34 @@
+package scheduleexample;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 发送延时消息
+ */
+public class ScheduleMessageProducer {
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
+        // 实例化一个生产者来产生延时消息
+        DefaultMQProducer producer = new DefaultMQProducer("wbs_schedule_mq_demo");
+        producer.setNamesrvAddr("localhost:9876");
+        // 启动生产者
+        producer.start();
+        int totalMessagesToSend = 100;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        for (int i = 0; i < totalMessagesToSend; i++) {
+            Message message = new Message("TopicTest", ("Hello scheduled message " + i + ", currentTimeLong=" + System.currentTimeMillis() + ", currentTime=" + sdf.format(new Date())).getBytes());
+            // 设置延时等级3，这个消息将在10s之后发送（现在只支持固定的几个时间，详情看delayTimeLevel）
+            message.setDelayTimeLevel(3);
+            // 发送消息
+            producer.send(message);
+        }
+        // 关闭生产者
+        producer.shutdown();
+    }
+}
